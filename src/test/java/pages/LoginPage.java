e65caebabdbd4762345a3433187a4aeed6c09336
Nariman Gumerov <nariman.gumerov@mail.ru>
2026-02-27 00:25:53 +0300

    Добавил классы

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,34 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class LoginPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    //Локаторы элементов
+    private By usernameField = By.id("user-name");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login-button");
+
+    //Конструктор класса - принимает драйвер и ждет
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    //Метод для открытия страницы логина
+    public void open() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    //Метод для выполнения авторизации
+    public void login(String user, String password) {
+        driver.findElement(usernameField).sendKeys(user);
+    }
+
+    //
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,32 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BaseTest {
+    // Объявляем переменные, которые будут доступны во всех классах-наследниках
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup(); //Настройка драйвера Chrome
+        driver = new ChromeDriver(); // Экземпляр браузера
+        driver.manage().window().maximize(); // Разворачивает окно на весь экран
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20)); //Создали объект явного ожидания на 20 сек
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null); {
+            driver.quit();
+        }
+    }
+
 }
